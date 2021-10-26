9f9afe36fce514dff3bd790b0b1e65623a1c8ec6
kalman kostenszky <ayeqql@inf.elte.hu>
2021-10-26 02:38:25 -0700

    color graph

--- src/app.py
+++ src/app.py
@@ -1,13 +1,11 @@
 # https://networkx.org/documentation/stable/tutorial.html
 import networkx as nx
 import matplotlib.pyplot as plt
-import agent as Agent
-import node as Node
+import agent as a
+import node as n
 import layout as Layout
-
-graph = nx.Graph()
-agent = Agent.Agent()
-graph.add_node(agent)
+from typing import List
+from typing import Tuple
 
 num29 = [1,1,1,1,1,1,1,0,1,1] #29
 num29.reverse()
@@ -15,16 +13,25 @@ num29.reverse()
 num5 = [0,0,0,0,1,1,1,0,1,1] #5
 num5.reverse()
 
-for i in range(10,0,-1):
-    node = Node.Node(i, str(num5[i-1]))
-    graph.add_node(node)
-    graph.add_edge(agent, node)
+def create_graph(child_count, pebbles = []) -> Tuple[nx.Graph, List[n.Node]]:
+    graph = nx.Graph()
+    agent = a.Agent()
+    graph.add_node(agent)
+    nodes = []
+    for i in range(0, child_count):
+        print(str(i) + " " + str(len(pebbles)))
+        pebble = '0' if len(pebbles) <= i else str(pebbles[i]) 
+        node = n.Node(i, pebble)
+        nodes.append(node)
+        graph.add_node(node)
+        graph.add_edge(agent, node, port_in=i)
+    return graph, agent, nodes
 
-agent.visit_neighbours(graph)
-print(agent.code)
-print(agent.to_port_number())
+graph, agent, neighbours = create_graph(10, [1,1])
+
+neighbours[0].is_agent_here = True
+
 
 pos = Layout.hierarchy_pos(graph, agent)
-nx.draw_networkx(graph, pos=pos)
+nx.draw_networkx(graph, pos=pos, node_color=['green'] + ['red' if node.is_agent_here else 'green' for node in neighbours])
 plt.show()
-
